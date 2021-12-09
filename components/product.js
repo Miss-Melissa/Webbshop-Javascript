7a5d2a37b178dd2dade240a0e7fd1a537df924a7
Celina <celina.johansson@hotmail.com>
2021-12-09 20:24:57 +0100

    Produkt mall
    Gjort produktmallen för kategorisidan

---
+++ components/product.js
@@ -0,0 +1,44 @@
+class Product extends HTMLElement {
+    constructor() {
+        super();
+
+    }
+    connectedCallback() {
+        console.log(this.data);
+
+        let productTitle = document.createElement("div");
+        productTitle.className = "product-title";
+        productTitle.innerText = this.data.title;
+
+        let wishlistBtn = document.createElement("div");
+        wishlistBtn.className = "bi bi-suit-heart";
+        wishlistBtn.addEventListener("click", (e) => {
+            console.log(e.target.className);
+            e.target.className = (e.target.className === "bi bi-suit-heart") ? "bi bi-suit-heart-fill" : "bi bi-suit-heart";
+        });
+
+        let productImg = document.createElement("img");
+        productImg.className = "product-img";
+        productImg.setAttribute("src", this.data.thumbnail)
+
+        let productPrice = document.createElement("div");
+        productPrice.className = "product-price";
+        productPrice.innerText = this.data.price + " kr";
+
+        let productShortDescription = document.createElement("div");
+        productShortDescription.className = "product-s-description";
+        productShortDescription.innerText = this.data.shortDescription;
+
+
+        this.appendChild(productTitle);
+        this.appendChild(wishlistBtn);
+        this.appendChild(productImg);
+        this.appendChild(productPrice);
+        this.appendChild(productShortDescription);
+
+        
+       
+    };
+}
+
+customElements.define('product-card', Product);

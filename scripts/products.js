317706b6dbe44b585b56f80c6d3877e94d4606ff
Celina <celina.johansson@hotmail.com>
2021-12-14 15:56:14 +0100

    La en kommentar
    Missade en kommentar som jag la till

--- scripts/products.js
+++ scripts/products.js
@@ -2,6 +2,7 @@ import ProductHandler from '../classes/ProductHandler.js';
 const ph = new ProductHandler();
 const url = new URL(location.href);
 
+//Ritar ut produkter baserat på category i en url
 const products = ph.getByCategory(url.searchParams.get('category'))
 
 products.forEach(product => {

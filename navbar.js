cf7fd85ad349162020892e8d5d21a2b3d15d1207
EddieWEB <eddie.andersson@medieinstitutet.se>
2021-12-15 20:19:51 +0100

    navbar och modal stylad redo för länkar

--- navbar.js
+++ navbar.js
@@ -1,39 +1,26 @@
-// function toggleHamburger() {
-//   const mainMenu = document.querySelector(".mainMenu");
-//   if (mainMenu.style.display === "flex") {
-//       mainMenu.style.display = "none";
-//   } else {
-//       mainMenu.style.display = "flex";
-//   }
-// }
 
-// document.querySelector(".hamburger").addEventListener("click", toggleHamburger)
+//--------------------------------------------------------------------------------
+// HTML reference
+const hamburgerModal = document.querySelector(".hamburger-modal");
+const hamburger = document.querySelector(".fa-bars");
+const closeModal = document.querySelector(".close-btn");
 
-// selectors
-const modal = document.querySelector(".varukorg-modal");
-const varukorgKnapp = document.querySelector(".fa-bars");
-const closeVarukorgModal = document.querySelector(".close-btn");
-
-//addeventlistener för att öppna modalen
-varukorgKnapp.addEventListener("click", openVarukorg);
-closeVarukorgModal.addEventListener("click", closeVarukorg);
-
-// addeventlistener för att stänga om man klickar utanför
+//--------------------------------------------------------------------------------
+// Handle clicks
+hamburger.addEventListener("click", openHamburger);
+closeModal.addEventListener("click", closeHamburger);
 document.addEventListener("click", outsideClick)
 
-//funktion för att öppna varukorgsmodal
-function openVarukorg () {
-    modal.style.display = "flex";
+//--------------------------------------------------------------------------------
+// Hamburger modal toggle functions
+function openHamburger () {
+    hamburgerModal.style.display = "flex";
 }
-
-//funktion för att stänga varukorgsmodal med stängknappen
-function closeVarukorg () {
-    modal.style.display = "none";
+function closeHamburger () {
+    hamburgerModal.style.display = "none";
 }
-
-//Funktion för att stänga varukorgsmodal om man klickar utanför varukorgen
 function outsideClick(click) {
-    if (click.target === modal) {
-        modal.style.display = "none";
+    if (click.target === hamburgerModal) {
+        hamburgerModal.style.display = "none";
     }
 }

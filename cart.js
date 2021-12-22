03970b86cf53ec2e862dbb71237a326e67837872
Celina <celina.johansson@hotmail.com>
2021-12-22 11:20:30 +0100

    Små saker
    lite småsakre som ej var commitade

--- cart.js
+++ cart.js
@@ -1,34 +1,3 @@
-// selectors modal
-const modal = document.querySelector(".varukorg-modal");
-const varukorgKnapp = document.querySelector(".fa-shopping-cart");
-const closeVarukorgModal = document.querySelector(".close-btn");
-
-//addeventlistener för att öppna modalen
-varukorgKnapp.addEventListener("click", openVarukorg);
-closeVarukorgModal.addEventListener("click", closeVarukorg);
-
-// addeventlistener för att stänga om man klickar utanför
-document.addEventListener("click", outsideClick)
-
-
-//funktion för att öppna varukorgsmodal
-function openVarukorg () {
-    modal.style.display = "block";
-}
-
-//funktion för att stänga varukorgsmodal med stängknappen
-function closeVarukorg () {
-    modal.style.display = "none";
-}
-
-//Funktion för att stänga varukorgsmodal om man klickar utanför varukorgen
-function outsideClick(click) {
-    if (click.target === modal) {
-        modal.style.display = "none";
-    }
-}
-
-//selectors varukorginnehåll
 const productList = document.querySelector("#product-list")
 const productName = document.querySelector("#product-name")
 const productPrice = document.querySelector("#product-price")
--- components/navbar.js
+++ components/navbar.js
@@ -1,66 +1,10 @@
-const navbarTemplate = document.createElement("template");
-
-navbarTemplate.innerHTML = `
-<nav>
-  <div class="nav-top">
-    <div class="nav-container">
-      <a class="nav-link home-logo" href="/"><img width="50" src="./Bilder/wie2-svg.svg"><span>WIE GAMING</span></a>
-      <span>
-        <a class="nav-link" href="#"><i class="bi bi-suit-heart-fill"></i></a>
-        <a id="nav-cart-btn" class="nav-link" href="#"><i class="bi bi-cart3"></i></a>
-        <a class="nav-link nav-burger" href="#"><i class="bi bi-justify"></i></a>
-      </span>
-    </div>
-  </div>
-
-  <div class="nav-bot">
-    <div class="nav-container">
-      <a class="nav-link" category="Skärmar" href="products.html?category=Skärmar">SKÄRMAR</a>
-      <a class="nav-link" category="Tangentbord" href="products.html?category=Tangentbord">TANGENTBORD</a>
-      <a class="nav-link" category="Möss" href="products.html?category=Möss">MÖSS</a>
-      <a class="nav-link" category="Konsoler" href="products.html?category=Konsoler">KONSOLER</a>
-      <a class="nav-link" category="Gamingstolar" href="products.html?category=Gamingstolar">GAMINGSTOLAR</a>
-      <a class="nav-link" category="Musmattor" href="products.html?category=Musmattor">MUSMATTOR</a>
-      <a class="nav-link" category="Hörlurar" href="products.html?category=Hörlurar">HÖRLURAR</a>
-    </div>
-  </div>
-</nav>
-<cart-modal></cart-modal>
-`;
-
 class NavBar extends HTMLElement {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.appendChild(navbarTemplate.content.cloneNode(true));
+        //Kod för HTML, antingen htmlkod i "" eller JS med DOM manipulering
      
-    this.querySelector('.nav-burger').addEventListener('click', (e) => {
-      let navBot = this.querySelector('.nav-bot');
-      navBot.style.display = navBot.style.display ? '' : 'block';
-    });
-
-    const url = new URL(location.href);
-    const productCategory = url.searchParams.get('category');
-    if(productCategory){
-      let activeElement = this.querySelector(`a[category="${productCategory}"]`);
-
-      if(activeElement)
-        activeElement.className += " link-active";
     }
-    
-  }
-
-  connectedCallback(){
-    const openModal = new Event('open-modal');
-
-    this.querySelector('#nav-cart-btn').addEventListener('click', () => {
-      document.dispatchEvent(openModal);
-    });
-  }
-
-
-
-
 }
 
 //'nav-bar' blir html elementet, i exempelSida.html ska det alltså stå <nav-bar></nav-bar>
